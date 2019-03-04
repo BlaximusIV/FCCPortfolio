ff7620ff71d07c1caa5ace4388e1b531f0452b1e
Neal Darley <darley.neal@gmail.com>
2019-03-04 08:28:48 -0700

    General outline.... may change.

---
+++ markdownpreviewer/src/Components/Body/Body.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Body extends React.Component {
+    render = () => 
+        <div>
+            
+        </div>
+}
+
+export default Body;
---
+++ markdownpreviewer/src/Components/Body/Preview.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent } from '*.svg';
+
+class Preview extends ReactComponent{
+
+
+
+}
+
+export default Preview;
